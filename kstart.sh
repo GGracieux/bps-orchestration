772da2dd7f3313c68bc05031510182732248d772
Guillaume Gracieux <ggracieux@gmail.com>
2018-04-02 10:13:42 +0200

    Renaming script

--- kstart.sh
+++ kstart.sh
@@ -3,12 +3,14 @@ gcloud config set project bagpipescores
 gcloud config set compute/zone europe-west1-b
 gcloud container clusters get-credentials bps-cluster
 
-# -- Deleting services
-kubectl delete service gateway-web
-kubectl delete service lilypond-web
-kubectl delete service midi2mp3-web
+# Instantiates deployments
+kubectl create -f lilypond-web-deployment.yaml
+kubectl create -f midi2mp3-web-deployment.yaml
+kubectl create -f gateway-web-deployment.yaml
 
-# Deleting deployments
-kubectl delete deployments gateway-web
-kubectl delete deployments lilypond-web
-kubectl delete deployments midi2mp3-web
+# Instantiates services for API pods
+kubectl create -f lilypond-web-service.yaml
+kubectl create -f midi2mp3-web-service.yaml
+
+# Instantiates gateway-web service with exposition on static IP pointed by bagpipe-scores.com DNS
+kubectl expose -f gateway-web-service.yaml --type "LoadBalancer" --load-balancer-ip='104.199.33.49'
